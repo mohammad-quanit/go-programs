ef84a3df9b8d67075730f9afe445557595b33022
Mohammad-Quanit <muhammadquanit@gmail.com>
2020-01-19 23:46:43 +0500

    pointers and types in Go

--- go-types/typings.go
+++ go-types/typings.go
@@ -23,21 +23,28 @@ func mutipleVals(x, y string) (string, string) {
 }
 
 func main() {
-	mathFunc()
-	// var num1, num2 float64 = 5.6, 100.55
-	num1, num2 := 5.6, 1000.66
-	fmt.Println(add(num1, num2))
+	var π float32 = 3.14
+	fmt.Println(5 + π)
 
-	w1, w2 := "Hey", "There"
-	fmt.Println(mutipleVals(w1, w2))
+	var maxUint32 uint32 = 4294967295  // Max uint32 size
+	maxUint32 = maxUint32 + 1
+  fmt.Println(maxUint32)
 
-	// explicitly type inference
-	var isInt = 65
-	var b float64 = float64(isInt)
-	fmt.Println(b)
+	// mathFunc()
+	// // var num1, num2 float64 = 5.6, 100.55
+	// num1, num2 := 5.6, 1000.66
+	// fmt.Println(add(num1, num2))
 
-	// implicitly type inference
-	isInt2 := 55
-	isFloat64 := isInt2
-	fmt.Println(isFloat64)
+	// w1, w2 := "Hey", "There"
+	// fmt.Println(mutipleVals(w1, w2))
+
+	// // explicitly type inference
+	// var isInt = 65
+	// var b float64 = float64(isInt)
+	// fmt.Println(b)
+
+	// // implicitly type inference
+	// isInt2 := 55
+	// isFloat64 := isInt2
+	// fmt.Println(isFloat64)
 }
