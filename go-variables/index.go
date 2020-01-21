aeabb99d2050358d29fe61053c7f80b2228f3d09
Mohammad-Quanit <muhammadquanit@gmail.com>
2020-01-21 23:26:47 +0500

    go arrays & slices

--- go-variables/index.go
+++ go-variables/index.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	/******** Variables & conditions ***********/
 	x := 5
 	var y int = 5
@@ -18,6 +18,17 @@ func main()  {
 	a[2] = 33
 	a = append(a, 66)
 	fmt.Println(a)
+
+	var myArr = [3]string{"blue coral", "staghorn coral", "pillar coral"}
+	fmt.Println(myArr)
+
+	/********* Slices or dynamic array *********************/
+	sliceArr := []int{1: -3, -2, -1, 0, 1, 2, 3}
+	fmt.Println(sliceArr)
+
+	floatSliceArr := []float64{3.14, 9.23, 111.11, 312.12, 1}
+	floatSliceArr = append(floatSliceArr, 555)
+	fmt.Println(floatSliceArr)
 
 	/************ Map / Object  *****************/
 	vertices := make(map[string]int)
